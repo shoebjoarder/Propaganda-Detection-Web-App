55898669e8264805d0ca7be83956f86053079c05
shoebjoarder <shoebjoarder@outlook.com>
2020-04-14 01:15:15 +0200

    Added bootstrap webpage

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ class PropagandaNews(object):
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 
 @app.route('/predict', methods=['POST'])
